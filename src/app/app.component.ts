d7fa92bb6eaae65625aaf414aee4c611f50d8605
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-04-02 10:10:00 +0200

    injecting dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { WishItem } from '../shared/models/wishItems';
 import { WishListComponent } from './wish-list/wish-list.component';
 import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
-import events from "../shared/services/EventService";
+import { EventService } from "../shared/services/EventService";
 
 @Component({
   selector: 'app-root',
@@ -25,7 +25,7 @@ export class AppComponent {
 
   title = 'Wishlist';
 
-constructor() {
+constructor(events: EventService) {
   events.listen("removeWish", (wish : any) => {
     let index = this.items.indexOf(wish);
     this.items.splice(index, 1);
