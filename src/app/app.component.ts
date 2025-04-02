134facb0059bdb1de67fbbafaf59a6d87c92ecaf
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-04-02 21:43:25 +0200

    issuing http requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -7,6 +7,7 @@ import { WishListComponent } from './wish-list/wish-list.component';
 import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
 import { EventService } from "../shared/services/EventService";
+import { WishService } from './wish.service';
 
 @Component({
   selector: 'app-root',
@@ -14,22 +15,24 @@ import { EventService } from "../shared/services/EventService";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  items : WishItem[] = [
-    new WishItem('Learn Angular'),
-    new WishItem('Get coffee', true),
-    new WishItem('Find grass that cuts itself')
-  ];
+export class AppComponent implements OnInit {
+  items! : WishItem[] ;
 
   filter: (item: WishItem) => boolean = () => true;
 
   title = 'Wishlist';
 
-constructor(events: EventService) {
+constructor(events: EventService, private wishService: WishService) {
   events.listen("removeWish", (wish : any) => {
     let index = this.items.indexOf(wish);
     this.items.splice(index, 1);
   });
   }
+
+ngOnInit(): void {
+    this.wishService.getWishes().subscribe((data: any) => {
+      this.items = data;
+  })
+}
 
 }
